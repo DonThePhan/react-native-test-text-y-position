53be19d5f7502a76a41fc040e89d33c8fd7c44dd
DonThePhan <donny@mapstrat.com>
2024-11-27 18:44:03 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useState, useEffect} from 'react';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
+  const [yPosition, setYPosition] = useState(null);
+
+  useEffect(() => {
+    console.log('yPosition => ', yPosition);
+  }, [yPosition]);
+
+  const handleLayout = event => {
+    const layout = event.nativeEvent.layout;
+    setYPosition(layout.y);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>1</Text>
+      {/* This is where we attach the onLayout prop */}
+      <Text>3</Text>
+      <Text onLayout={handleLayout}>2</Text>
+      <StatusBar style='auto' />
+      {yPosition !== null && <Text>Y Position of Text 2: {yPosition}</Text>}
     </View>
   );
 }
@@ -15,6 +31,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
